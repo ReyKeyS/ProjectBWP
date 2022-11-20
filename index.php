6537f604b7676c944d1971a83e8a29c1c9710117
Grady Hermawan <Gradyhermawan@gmail.com>
2022-11-20 23:26:56 +0700

    tambah email

--- index.php
+++ index.php
@@ -13,8 +13,13 @@
 </head>
 <body>
     <form action="">
-        <div class="container mx-auto">
-            <h1 class="text-center font-bold text-6xl bg-gradient-to-r from-blue-500 to-teal-300 bg-clip-text text-transparent">WELCOME TO MOBILE LEGEND</h1>
+        <div class="container mx-auto flex-col fixed top-52 left-52 border rounded-xl place-content-center">
+            <h1 class="text-center font-bold text-6xl bg-gradient-to-r from-blue-500 to-teal-300 bg-clip-text text-transparent">LOGIN</h1>
+            <span>Email</span>
+            <label for="email" id="email">
+                <input type="email" placeholder="masukkan email" class="px-3 py-2 border shadow-lg rounded w-full text-sm focus:ring-2 focus:ring-green-400 focus:outline-none invalid:text-purple-600 invalid:focus:ring-purple-600 peer">
+                <p class="text-sm text-purple-600 invisible peer-invalid:visible">Email tidak valid</p>
+            </label>
         </div>
     </form>
 </body>
