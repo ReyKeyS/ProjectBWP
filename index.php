01c98209117515b73b1944f803c3935196cf5030
Paddy Hermanito <paddy.hermanito.suhartanto93@gmail.com>
2022-11-20 22:50:33 +0700

    coba tailwind

--- index.php
+++ index.php
@@ -9,8 +9,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-    ahah
+    <form action="">
+        <div class="container mx-auto">
+            <h1 class="text-center font-bold text-6xl bg-gradient-to-r from-blue-500 to-teal-300 bg-clip-text text-transparent">WELCOME TO MOBILE LEGEND</h1>
+        </div>
+    </form>
 </body>
 </html>
