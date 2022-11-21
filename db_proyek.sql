e709ed23523ec1ccb9655653b5aa4cf9d0aa82aa
PaddyHermanito <paddy.hermanito.suhartanto93@gmail.com>
2022-11-21 14:05:42 +0700

    Database

---
+++ db_proyek.sql
@@ -0,0 +1,43 @@
+/*
+SQLyog Community v13.1.9 (64 bit)
+MySQL - 10.4.25-MariaDB : Database - db_proyek
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`db_proyek` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `db_proyek`;
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id_users` varchar(200) NOT NULL,
+  `nama` varchar(200) DEFAULT NULL,
+  `email` varchar(200) DEFAULT NULL,
+  `telp` varchar(100) DEFAULT NULL,
+  `alamat` varchar(200) DEFAULT NULL,
+  `password` varchar(200) DEFAULT NULL,
+  `status` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id_users`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `users` */
+
+insert  into `users`(`id_users`,`nama`,`email`,`telp`,`alamat`,`password`,`status`) values 
+('US0001','Ryan','ryk@gmail.com','081234567891','Ngangel','123',1),
+('US0002','paddy','paddy@mail.com','089512753','Jl medayu selaran','111',1);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
