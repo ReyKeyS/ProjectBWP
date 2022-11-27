e049bacabd0959c7fdd08e3e53b454f79057bb42
Paddy Hermanito <paddy.hermanito.suhartanto93@gmail.com>
2022-11-27 13:56:28 +0700

    Update history.php

--- history.php
+++ history.php
@@ -95,10 +95,97 @@
                 }
             ?>
         </nav>
-        <div class="w-11/12 mx-auto pt-20 bg-red-300 flex flex-col">
+        <div class="w-11/12 mx-auto pt-20 mb-auto flex flex-col">
             <div class="text-4xl font-semibold">TRANSACTION HISTORY</div>
+            <div class="w-1/2 h-64 mx-auto my-3 flex border rounded-xl">
+                <div class="w-1/4 flex flex-col">
+                    <div class="text-slate-400">KODEINVOICE123</div>
+                    <img src="assets/gonadi.jpg" alt="Imaged" class="w-48 h-48 m-auto">
+                </div>
+                <div class="w-2/4 flex flex-col pt-20">
+                    <div class="text-3xl font-semibold">Image Title</div>
+                    <div class="text-base">1 x Rp 120.000</div>
+                </div>
+                <div class="w-1/4 flex flex-col pt-20">
+                    <div>Total</div>
+                    <div class="text-xl font-bold">Rp 120.000</div>
+                    <a class="text-lg font-semibold mt-auto bg-gradient-to-r from-purple-700 to-blue-600 bg-clip-text text-transparent hover:bg-gradient-to-r hover:from-purple-900 hover:to-blue-800 hover:bg-clip-text hover:text-transparent hover:cursor-pointer">Lihat Detail Transaksi</a>
+                </div>
+            </div>
+            <div class="w-1/2 h-64 mx-auto my-3 flex border rounded-xl">
+                <div class="w-1/4 flex flex-col">
+                    <div class="text-slate-400">KODEINVOICE123</div>
+                    <img src="assets/gonadi.jpg" alt="Imaged" class="w-48 h-48 m-auto">
+                </div>
+                <div class="w-2/4 flex flex-col pt-20">
+                    <div class="text-3xl font-semibold">Image Title</div>
+                    <div class="text-base">1 x Rp 120.000</div>
+                </div>
+                <div class="w-1/4 flex flex-col pt-20">
+                    <div>Total</div>
+                    <div class="text-xl font-bold">Rp 120.000</div>
+                    <a class="text-lg font-semibold mt-auto bg-gradient-to-r from-purple-700 to-blue-600 bg-clip-text text-transparent hover:bg-gradient-to-r hover:from-purple-900 hover:to-blue-800 hover:bg-clip-text hover:text-transparent hover:cursor-pointer">Lihat Detail Transaksi</a>
+                </div>
+            </div>
+            <div class="w-1/2 h-64 mx-auto my-3 flex border rounded-xl">
+                <div class="w-1/4 flex flex-col">
+                    <div class="text-slate-400">KODEINVOICE123</div>
+                    <img src="assets/gonadi.jpg" alt="Imaged" class="w-48 h-48 m-auto">
+                </div>
+                <div class="w-2/4 flex flex-col pt-20">
+                    <div class="text-3xl font-semibold">Image Title</div>
+                    <div class="text-base">1 x Rp 120.000</div>
+                </div>
+                <div class="w-1/4 flex flex-col pt-20">
+                    <div>Total</div>
+                    <div class="text-xl font-bold">Rp 120.000</div>
+                    <a class="text-lg font-semibold mt-auto bg-gradient-to-r from-purple-700 to-blue-600 bg-clip-text text-transparent hover:bg-gradient-to-r hover:from-purple-900 hover:to-blue-800 hover:bg-clip-text hover:text-transparent hover:cursor-pointer">Lihat Detail Transaksi</a>
+                </div>
+            </div>
+            
             
         </div>
+        <nav class="h-96 bg-black">
+            <div class="flex">
+                <div class="w-1/2 pt-5 flex flex-col border-r">
+                    <div class="flex justify-center my-1">
+                        <div class="text-white mr-5 text-4xl font-semibold">Send Us Mail</div>
+                        <img src="assets/email (1).png" alt="">
+                    </div>
+                    <input type="text" name="email" placeholder="Email Address" class="px-3 py-1 my-1 w-96 mx-auto rounded-lg bg-transparent placeholder-white border-b-2 focus:ring-2 focus:ring-purple-400 focus:outline-none focus:border-b-0">
+                    <div class="mx-auto text-white my-3">
+                        <div class="text-white">Mail</div>
+                        <textarea name="mail" id="" cols="50" rows="5" class="rounded-xl focus:ring-2 focus:ring-purple-400 focus:outline-none"></textarea>
+                        <div class="flex">
+                            <input type="checkbox" name="send">
+                            <div class="my-auto">Let us send you an email</div>
+                            <button type="submit" name="send" class="px-3 py-2 ml-auto rounded-xl font-semibold bg-gradient-to-r from-purple-700 to-blue-600 hover:bg-gradient-to-r hover:from-purple-900 hover:to-blue-800">Send</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-1/2 flex flex-col pt-2">
+                    <div class="my-3 text-white text-4xl flex font-semibold justify-center">Follow Us</div>
+                    <div class="flex space-x-4 place-content-center">
+                        <a href="https://www.tokopedia.com/" target="_blank" class="hover:scale-125 duration-200">
+                            <img src="assets/tokopedia.png" alt="" class="w-10 h-10">
+                        </a>
+                        <a href="https://www.instagram.com" target="_blank" class="hover:scale-125 duration-200">
+                            <img src="assets/instagram.png" alt="" class="w-10 h-10">
+                        </a>
+                        <a href="https://www.facebook.com/" target="_blank" class="hover:scale-125 duration-200">
+                            <img src="assets/facebook.png" alt="" class="w-10 h-10">
+                        </a>
+                        <a href="https://www.tiktok.com" target="_blank" class="hover:scale-125 duration-200">
+                            <img src="assets/tiktok.png" alt="" class="w-10 h-10">
+                        </a>
+                    </div>
+                    <div class="mt-7 mb-5 text-white text-4xl flex font-semibold justify-center">Contacts</div>
+                    <div class="text-white text-center mx-auto text-sm"><span class="text-lg font-bold"><i>Customer Care</i></span><br><b>Email:</b> Glorindo.Care@care.co.id</div>
+                    <div class="text-white text-center mx-auto text-sm"><span class="text-lg font-bold"><i>Contact Person</i></span><br><b>Email:</b> Glorindo.Komp@official.co.id<br><b>Phone:</b> 081234567890</div>
+                </div>
+            </div>
+            <div class="text-white text-center mt-5">&copy; Glorindo Komputer Inc. 2022 All Rights Reserved</div>
+        </nav>
     </form>
 </body>
 </html>
