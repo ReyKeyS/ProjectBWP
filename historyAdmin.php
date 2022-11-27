e645ef89f2b95bc409b9ad083e5207d3c942aea1
ReyKeyS <104489667+ReyKeyS@users.noreply.github.com>
2022-11-27 21:23:04 +0700

    Gnti TITLE HTML

--- historyAdmin.php
+++ historyAdmin.php
@@ -15,7 +15,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home Admin</title>
+    <title>History Admin</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
         tailwind.config = {
